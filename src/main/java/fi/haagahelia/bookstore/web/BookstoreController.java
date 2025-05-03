2300674756a7963abca56b00eae695f638ed1924
Tu Nguyen <nguyendoductu@gmail.com>
2025-05-03 06:31:17 +0300

    Added add and delete functionality

--- src/main/java/fi/haagahelia/bookstore/web/BookstoreController.java
+++ src/main/java/fi/haagahelia/bookstore/web/BookstoreController.java
@@ -1,13 +1,15 @@
 package fi.haagahelia.bookstore.web;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
+
+import fi.haagahelia.bookstore.domain.Book;
 import fi.haagahelia.bookstore.domain.BookRepository;
+
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
 
 @Controller
 public class BookstoreController {
@@ -17,9 +19,27 @@ public class BookstoreController {
         this.repository = repository;
     }
 
-    @RequestMapping(value = {"/", "booklist"})
+    @RequestMapping(value = {"/", "/booklist"})
     public String bookList(Model model) {
         model.addAttribute("books", repository.findAll());
         return "booklist";
+    }
+
+    @RequestMapping(value = "/addbook")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save(Book book) {
+        repository.save(book);
+        return "redirect:booklist";
+    }
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+        repository.deleteById(bookId);
+        return "redirect:../booklist";
     }
 }
