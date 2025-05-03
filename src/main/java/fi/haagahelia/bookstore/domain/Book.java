58724512401127e6621d964ab8f011f805dabd22
Tu Nguyen <nguyendoductu@gmail.com>
2025-05-03 07:25:16 +0300

    Add bootstrap

--- src/main/java/fi/haagahelia/bookstore/domain/Book.java
+++ src/main/java/fi/haagahelia/bookstore/domain/Book.java
@@ -8,13 +8,11 @@ import jakarta.persistence.Id;
 @Entity
 public class Book {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    private String title;
-    private String author;
-    private String isbn;
-    private int publicationYear;
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private long id; 
+    private String title, author, isbn;
     private double price;
+    private int publicationYear;
 
     public Book() {}
 
@@ -26,25 +24,47 @@ public class Book {
         this.price = price;
     }
 
-    // Getters
-    public Long getId() { return id; }
-    public String getTitle() { return title; }
-    public String getAuthor() { return author; }
-    public String getIsbn() { return isbn; }
-    public int getPublicationYear() { return publicationYear; }
-    public double getPrice() { return price; }
-
-    // Setters
-    public void setId(Long id) { this.id = id; }
-    public void setTitle(String title) { this.title = title; }
-    public void setAuthor(String author) { this.author = author; }
-    public void setIsbn(String isbn) { this.isbn = isbn; }
-    public void setPublicationYear(int publicationYear) { this.publicationYear = publicationYear; }
-    public void setPrice(double price) { this.price = price; }
+    // Getters and Setters
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public String getIsbn() {
+        return isbn;
+    }
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
 
     @Override
     public String toString() {
-        return "Book [id=" + id + ", title=" + title + ", author=" + author + ", isbn=" + isbn +
-               ", publicationYear=" + publicationYear + ", price=" + price + "]";
+        return "Book [id=" + id + ", title=" + title + ", author=" + author + ", isbn=" + isbn + ", publicationYear="
+                + publicationYear + ", price=" + price + "]";    
     }
 }
